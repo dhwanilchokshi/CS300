8528af00868cbc70831e917539121db7f0fd57ca
ZackSalah <ms24@ada.cs.pdx.edu>
2017-11-25 18:33:15 -0800

    new terminal v1

--- terminal.cpp
+++ terminal.cpp
@@ -13,13 +13,47 @@ int Terminal::read()
 }
 int Terminal::menu(int user_type)
 {
+    int choice = 0;
+    bool INmenu = false;
     cout<<"Welcome -name-\n"
         <<"//////////////////////////////////////////////"<<endl;
     do 
     {
+        INmenu = false;
         cout<<"What would you like to do? - Please choose a number corresponding to the menu item:"<<endl;
-    }while(1);
+        cout<<menues[user_type];
+        cin>>choice;
+        cin.ignore(100,'\n');
+        if(!choice);
+        else if((user_type == 0) && (choice < 4 && choice > 0))
+            providers(choice);
+        else if((user_type == 1) && (choice < 3 && choice > 0))
+            managers(choice);
+        else if((user_type == 2) && (choice < 5 && choice > 0))
+            operators(choice);
+        else
+            INmenu = true;
+    }while(INmenu);
+    cout<<"\n~~~LOGOUT~~~\n";
     return 1;
+}
+void Terminal::providers(int choice)
+{
+    if(choice == 1);
+    else if(choice == 2);
+    else;
+}
+void Terminal::managers(int choice)
+{
+    if(choice == 1);
+    else;
+}
+void Terminal::operators(int choice)
+{
+    if(choice == 1);
+    else if(choice == 2);
+    else if(choice == 3);
+    else;
 }
 int Terminal::member_number_validation(int user_entry)
 {return 1;}
--- terminal.h
+++ terminal.h
@@ -2,7 +2,7 @@
 #include <cstring>
 #include <ctime>
 #include <fstream>
-const char menu[][150]= {"1- provide a service to a member\n2 - Look up a service code\n3 - Request a copy of provider directory\n","1 - View summary report\n2 - Run individual reports\n","1 - Add new members\n2 - Add new providers\n3 - Update\n4 - Delete\n"};
+const char menues[][150]= {"0 - QUIT\n1 - provide a service to a member\n2 - Look up a service code\n3 - Request a copy of provider directory\n","0 - QUIT\n1 - View summary report\n2 - Run individual reports\n","0 - QUIT\n1 - Add new members\n2 - Add new providers\n3 - Update\n4 - Delete\n"};
 class Terminal
 {
     public:
@@ -13,6 +13,9 @@ class Terminal
 
         //User specific menus
         int menu(int user_type);
+        void providers(int choice);
+        void managers(int choice);
+        void operators(int choice);
 
     protected:
         //User number validation functions
